8b2279700c13a50179849d1fecf8e26c4bfe52a3
Chukharev Fedor <randomfedor@gmail.com>
2018-10-16 16:04:25 +0300

    Implement non-LINQ parts of ResearcherList class

--- lab1_v4/Program.cs
+++ lab1_v4/Program.cs
@@ -7,27 +7,7 @@ namespace lab1_v4
     {
         static void Main(string[] args)
         {
-            Researcher researcher = new Researcher("Petrov", "Ivan", 1.2);
-            researcher.AddProject(new LocalProject("Loc1"), new InternationalProject("Int1", ProjectType.Fundamental, new DateTime(), "Russia", 1), new InternationalProject("Int2", ProjectType.Applied, new DateTime(), "Russia2", 3));
-            /*Console.WriteLine(researcher.ToString());
-            Project project1 = new Project("Physics", ProjectType.Applied);
-            Project project2 = new Project("Physics", ProjectType.Applied);
-            Project project3 = null;
-            Console.WriteLine(project1 == project2);
-            Console.WriteLine(project1.Equals(project2));
-            Console.WriteLine(project1.Equals(project3));
-            Console.WriteLine(project3 == project1);
-            Console.WriteLine(project3 == null);*/
-            foreach (Project i in researcher)
-            {
-                Console.WriteLine(i.ToString());
-            }
-            Console.ReadKey();
-            foreach (Project i in researcher.GetIntBigProjects())
-            {
-                Console.WriteLine(i.ToString());
-            }
-            Console.ReadKey();
+            //TODO: Complete third assignment
         }
     }
 }
---
+++ lab1_v4/ResearcherList.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace lab1_v4
+{
+    class ResearcherList
+    {
+        private List<Researcher> researchers;
+        ResearcherList(List<Researcher> researchers = null)
+        {
+            if (researchers == null)
+                researchers = new List<Researcher>();
+
+            this.researchers = researchers;
+        }
+        public void AddDefaults()
+        {
+            //TODO: Add minimal set of researchers viable for testing
+        }
+        public override string ToString()
+        {
+            string result = "";
+            string delim = "\n";
+            int counter = 1;
+            foreach (Researcher researcher in researchers)
+            {
+                result += counter +": " + researcher + delim;
+                counter++;
+            }
+            return result;
+        }
+        public override int GetHashCode()
+        {
+            return researchers.GetHashCode();
+        }
+    }
+}
